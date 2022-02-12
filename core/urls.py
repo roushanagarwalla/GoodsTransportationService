3cd86429080c57fe50b1b82cfb8b723552fd4781
Roushan Agarwalla <roshanagarwal375@gmail.com>
2022-02-12 12:19:34 +0530

    Added Signup and Login

--- core/urls.py
+++ core/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
+    path('dealer/', views.index_dealer, name="index_dealer"),
+    path('driver/', views.index_driver, name="index_driver"),
     path('signup/', views.signup, name="signup"),
     path('signup/driver', views.signup_driver, name="signup_driver"),
     path('signup/dealer', views.signup_dealer, name="signup_dealer"),
-    path('login/', views.login, name="login"),
+    path('login/', views.login_user, name="login_user"),
     path('login/driver', views.login_driver, name="login_driver"),
     path('login/dealer', views.login_dealer, name="login_dealer"),
     path('logout/', views.logout, name="logout"),
--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
-from .models import Dealer
+from django.db.models import Q
+from .models import Dealer, Driver
+from django.contrib.auth import authenticate, login, logout
 
 def signup(request):
     return render(request, "signup/signup.html")
@@ -18,28 +20,87 @@ def signup_dealer(request):
         to_city = request.POST.get("to_city")        
         pass1 = request.POST.get("pass1")        
         pass2 = request.POST.get("pass2")
-        # if(pass1==pass2):
-        # user = User.objects.create_user(mob, email, pass1)
-        user = User.objects.get(username = mob)
-        # print(user) 
+        user = User.objects.create_user(email, email, pass1)
+        user.first_name = "dealer"
+        user.save()
         dealer = Dealer(rel=user, name=name, mob=mob, nature=nature, weight=weight, from_state=from_state, from_city=from_city, to_state = to_state, to_city=to_city)
         dealer.save()
+        redirect("/login/dealer")
     return render(request, "signup/signup_dealer.html")
 
 def signup_driver(request):
+    if request.method == "POST":
+        name = request.POST.get("name")        
+        email = request.POST.get("email")        
+        mob = request.POST.get("mob")        
+        pass1 = request.POST.get("pass1")        
+        pass2 = request.POST.get("pass2")
+        age = request.POST.get("age")        
+        exp = request.POST.get("exp")        
+        tname = request.POST.get("tname")        
+        truck_no = request.POST.get("truck_no")        
+        capacity = request.POST.get("capacity")        
+        
+        from_state1 = request.POST.get("from_state1")        
+        from_city1 = request.POST.get("from_city1")        
+        to_state1 = request.POST.get("to_state1")        
+        to_city1 = request.POST.get("to_city1")   
+        
+        from_state2 = request.POST.get("from_state2")        
+        from_city2 = request.POST.get("from_city2")        
+        to_state2 = request.POST.get("to_state2")        
+        to_city2 = request.POST.get("to_city2")   
+
+        from_state3 = request.POST.get("from_state3")        
+        from_city3 = request.POST.get("from_city3")        
+        to_state3 = request.POST.get("to_state3")        
+        to_city3 = request.POST.get("to_city3") 
+
+        user = User.objects.create_user(email, email, pass1)
+        user.first_name = "driver"
+        user.save()
+        
+        driver = Driver(rel=user, name=name, mob=mob, age=age,driving_experience = exp , truck_no=truck_no, truck_capacity=capacity, transporter_name= tname,from_state1=from_state1, from_city1=from_city1, to_state1 = to_state1, to_city1=to_city1, from_state2=from_state2, from_city2=from_city2, to_state2 = to_state2, to_city2=to_city2, from_state3=from_state3, from_city3=from_city3, to_state3 = to_state3, to_city3=to_city3)
+        driver.save()
+        redirect("/login/driver")
+    
     return render(request, "signup/signup_driver.html")
 
-def login(request):
+def login_user(request):
     return render(request, "login/login.html")
 
 def login_dealer(request):
+    if request.method == "POST":
+        email = request.POST.get("email");
+        password = request.POST.get("pass");
+        user = authenticate(username=9678388634, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("/dealer")
     return render(request, "login/login_dealer.html")
 
 def login_driver(request):
+    if request.method == "POST":
+        email = request.POST.get("email");
+        password = request.POST.get("pass");
+        user = authenticate(username=9678388634, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("/driver")
     return render(request, "login/login_driver.html")
 
 def logout(request):
     return redirect("/login")
 
-def index(request):
-    return render(request, "index.html")
+def index_dealer(request):
+    print(request.user)
+    dealer = Dealer.objects.filter(rel = request.user).first()
+    drivers = Driver.objects.filter(Q(from_city1=dealer.from_city, to_city1=dealer.to_city) | Q(from_city2=dealer.from_city, to_city2=dealer.to_city) | Q(from_city2=dealer.from_city, to_city2=dealer.to_city))
+    print(drivers)
+    return render(request, "index_dealer.html", context={
+        "drivers": drivers,
+    })
+
+def index_driver(request):
+    print(request.user)
+    return render(request, "index_driver.html")
